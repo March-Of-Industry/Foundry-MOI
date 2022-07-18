6689b43d5f27f10d33ac3029b823141d59441ce5
Superfrogman98 <frogman98m@aol.com>
2022-07-17 23:06:46 -0500

    Migrate to universal buildscript

---
+++ src/main/java/exter/foundry/Tags.java
@@ -0,0 +1,9 @@
+package com.exter.foundry;
+
+public class Tags
+{
+  public static final String MOD_ID = "GRADLETOKEN_MODID";
+  public static final String MOD_NAME = "GRADLETOKEN_MODNAME";
+  public static final String MOD_VERSION = "GRADLETOKEN_VERSION";
+  public static final String GROUPNAME = "GRADLETOKEN_GROUPNAME";
+}
